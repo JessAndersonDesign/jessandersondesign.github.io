f6f50fad28d0c65382e5a525cf23d8c848ac41c1
Patrick K. Clark <dev@pkclark.com>
2023-09-04 14:58:53 -0700

    fix build... again

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 sass scss/styles.scss:css/styles.css
-echo '' > js/scripts.min.js
+echo '' > js/scripts.concat.js
 cat js/jquery.min.js >> js/scripts.concat.js
 cat js/t.min.js >> js/scripts.concat.js
 cat js/scripts.js >> js/scripts.concat.js
 cat js/portfolio.js >> js/scripts.concat.js
-terser --compress --mangle -- js/scripts.concat.js >> js/scripts.min.js
+terser --compress --mangle -- js/scripts.concat.js > js/scripts.min.js
